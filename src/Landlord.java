8d59d6f0639a242308fa2ce7bfedf39188e5b5ec
robypz <robertyepez0208@hotmail.com>
2026-03-16 14:48:44 +0100

    Store and Update methods on LandLord Model

--- src/Landlord.java
+++ src/Landlord.java
@@ -1,4 +1,7 @@
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class Landlord {
     private int id;
@@ -7,11 +10,77 @@ public class Landlord {
     private String dni;
     private Date birth_date;
 
-    public Landlord(String name, String last_name, String dni, Date birth_date) {
-        this.name = name;
-        this.last_name = last_name;
-        this.dni = dni;
-        this.birth_date = birth_date;
+    //meotodos para operar tabla en db
+    public static List<Landlord> index (){
+        List<Landlord> landlords = new ArrayList<>();
+        return landlords;
+    }
+    public void store () throws SQLException {
+        String sql = "INSERT INTO landlords (name, last_name, dni, birth_date) VALUES (?, ?, ?, ?)";
+
+        try (
+                Connection conn = MariaDB.getConnection();
+                PreparedStatement stmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)
+        ) {
+
+            stmt.setString(1,this.getName());
+            stmt.setString(2,this.getLast_name());
+            stmt.setString(3,this.getDni());
+            stmt.setDate(4, new java.sql.Date(this.getBirth_date().getTime()));
+            stmt.setInt(5,this.getId());
+
+            stmt.executeUpdate();
+
+            try (ResultSet keys = stmt.getGeneratedKeys()) {
+                if (keys.next()) {
+                    this.setName(keys.getString("name"));
+                    this.setLast_name(keys.getString("last_name"));
+                    this.setDni(keys.getString("dni"));
+                    this.setBirth_date(keys.getDate("birth_date"));
+                }
+            }
+
+        } catch (SQLException e) {
+            IO.println(e.getMessage());
+        }
+    }
+
+    public void update (){
+        String sql = "UPDATE landlords SET name = ?, last_name = ?, dni = ?, birth_date = ? WHERE id = ?";
+
+        try (Connection conn = MariaDB.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setString(1,this.getName());
+            stmt.setString(2,this.getLast_name());
+            stmt.setString(3,this.getDni());
+            stmt.setDate(4, new java.sql.Date(this.getBirth_date().getTime()));
+            stmt.setInt(5,this.getId());
+
+            stmt.executeUpdate();
+
+            try (ResultSet keys = stmt.getGeneratedKeys()) {
+                if (keys.next()) {
+                    this.setName(keys.getString("name"));
+                    this.setLast_name(keys.getString("last_name"));
+                    this.setDni(keys.getString("dni"));
+                    this.setBirth_date(new Date(keys.getDate("birth_date").getTime()));
+                }
+            }
+
+        } catch (SQLException e) {
+            IO.println(e.getMessage());
+        }
+
+    }
+
+    public static Landlord show (int id){
+        Landlord landlord = new Landlord();
+        return landlord;
+    }
+
+    public static void delete (Landlord landlord){
+
     }
 
     public int getId() {
