9497e9ce109cbf8676cdbcc919b8707fe0b11bfd
robypz <robertyepez0208@hotmail.com>
2026-03-16 13:08:16 +0100

    MariaDB Connection

---
+++ src/MariaDB.java
@@ -0,0 +1,13 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MariaDB {
+    private static final String URL = "jdbc:mariadb://localhost:3306/rent";
+    private static final String USER = "root";
+    private static final String PASSWORD = "4k3l4rr3";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
