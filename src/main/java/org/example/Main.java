b2b40a98c83e00082f9abae37a006c7079815d9e
robypz <robertyepez0208@hotmail.com>
2026-04-21 10:37:59 +0200

    feat: initialize Main class with HomeController and LandlordController dependencies

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,18 +2,13 @@ package org.example;
 
 import controllers.HomeController;
 import controllers.LandlordController;
-import views.LandlordView;
-
-import java.sql.SQLException;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static void main() {
-        LandlordController landlordController = new LandlordController();
         HomeController homeController = new HomeController(
-                landlordController
-        );
+                new LandlordController());
     }
 
 }
